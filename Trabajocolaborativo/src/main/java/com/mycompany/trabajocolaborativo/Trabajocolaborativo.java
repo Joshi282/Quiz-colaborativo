8ec5daabac680ea6c9f22113a870e97dc4f25779
AlexaMiranda30 <milalex3003@gmail.com>
2025-09-02 20:52:53 -0600

    Arreglo de la rama

--- Trabajocolaborativo/src/main/java/com/mycompany/trabajocolaborativo/Trabajocolaborativo.java
+++ Trabajocolaborativo/src/main/java/com/mycompany/trabajocolaborativo/Trabajocolaborativo.java
@@ -4,13 +4,37 @@
 
 package com.mycompany.trabajocolaborativo;
 
+ import java.util.Scanner;
 /**
  *
  * @author almi0
  */
 public class Trabajocolaborativo {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public Trabajocolaborativo() {
     }
-}
+    
+
+  public void ejercicio(){
+      Scanner sc = new Scanner(System.in);
+      
+      System.out.print("Primer numero: ");
+      int a = sc.nextInt();
+      System.out.print("segundo numero:");
+      int b = sc.nextInt();
+      int resultado;
+      if(a == b){
+      resultado = a * b;
+      } else if (a > b){
+         resultado = a - b;
+         
+      } else{
+          resultado = a + b;
+          
+      }
+      
+      System.out.println("Resultado: " + resultado);
+      
+    }
+    
+    }
